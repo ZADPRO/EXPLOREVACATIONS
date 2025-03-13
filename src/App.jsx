783663b6d8be8be864659bdb8c2aa02887bdcb48
IONHAX04 <thirukumara04@gmail.com>
2025-03-13 22:31:16 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainRoutes from "./components/00-MainRoutes/MainRoutes";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MainRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/00-MainRoutes/MainRoutes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "../05-Header/Header";
+import Footer from "../06-Footer/Footer";
+import Home from "../01-Home/Home";
+import Tours from "../02-Tours/Tours";
+import Cars from "../03-Cars/Cars";
+import Contact from "../04-Contact/Contact";
+
+export default function MainRoutes() {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
