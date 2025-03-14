ced7befdde75e3da1807351783b6ef584ecd91b1
IONHAX04 <thirukumara04@gmail.com>
2025-03-14 15:27:19 +0530

    home drafted 03

--- src/components/01-Home/Home.jsx
+++ src/components/01-Home/Home.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 
 import { Send, CarFront, TicketsPlane } from "lucide-react";
+import { Button } from "primereact/button";
+import { Dropdown } from "primereact/dropdown";
+import { Calendar } from "primereact/calendar";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputText } from "primereact/inputtext";
 
 export default function Home() {
   const [tabSelected, setTabSelected] = useState({
@@ -57,6 +62,18 @@ export default function Home() {
     };
   });
 
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+
+  const cities = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+
   return (
     <div className="">
       <div className="homePageContainer01">
@@ -64,7 +81,7 @@ export default function Home() {
       </div>
       <div className="homePageContainer02 relative">
         <section
-          className="w-full md:w-8/12 mx-auto mt-[-40px] shadow-xl bg-white rounded-md"
+          className="w-11/12 md:w-8/12 mx-auto mt-[-40px] shadow-xl bg-white rounded-md"
           aria-multiselectable="false"
         >
           <ul
@@ -74,7 +91,7 @@ export default function Home() {
           >
             <li className="" role="presentation">
               <button
-                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-6 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
+                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-3 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
                   tabSelected.currentTab === 1
                     ? "border-[#02569c] stroke-[#02569c] text-[#02569c] hover:border-[#02569c]  hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:border-slate-500"
                     : "justify-self-center border-transparent stroke-slate-700 text-slate-700 hover:border-[#02569c] hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:text-slate-500"
@@ -100,7 +117,7 @@ export default function Home() {
             </li>
             <li className="" role="presentation">
               <button
-                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-6 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
+                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-3 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
                   tabSelected.currentTab === 2
                     ? "border-[#02569c] stroke-[#02569c] text-[#02569c] hover:border-[#02569c]  hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:border-slate-500"
                     : "justify-self-center border-transparent stroke-slate-700 text-slate-700 hover:border-[#02569c] hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:text-slate-500"
@@ -124,7 +141,7 @@ export default function Home() {
             </li>
             <li className="" role="presentation">
               <button
-                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-6 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
+                className={`-mb-px inline-flex h-12 w-full items-center justify-center gap-2 whitespace-nowrap rounded-t border-b-2 px-3 text-sm font-medium tracking-wide transition duration-300 hover:bg-emerald-50 hover:stroke-[#02569c] focus:bg-emerald-50 focus-visible:outline-none disabled:cursor-not-allowed ${
                   tabSelected.currentTab === 3
                     ? "border-[#02569c] stroke-[#02569c] text-[#02569c] hover:border-[#02569c]  hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:border-slate-500"
                     : "justify-self-center border-transparent stroke-slate-700 text-slate-700 hover:border-[#02569c] hover:text-[#02569c] focus:border-[#02569c] focus:stroke-[#02569c] focus:text-[#02569c] disabled:text-slate-500"
@@ -162,12 +179,47 @@ export default function Home() {
               aria-labelledby="tab-label-1ai"
               tabindex="-1"
             >
-              <p>
-                What is the recipe for successful achievement? To my mind there
-                are just four essential ingredients: Choose a career you love,
-                give it the best there is in you, seize your opportunities, and
-                be a member of the team.
-              </p>
+              <div className="flex gap-3">
+                <div className="p-inputgroup flex-1">
+                  <span className="p-inputgroup-addon">
+                    <i className="pi pi-map-marker"></i>
+                  </span>
+                  <Dropdown
+                    value={selectedCity}
+                    onChange={(e) => setSelectedCity(e.value)}
+                    options={cities}
+                    optionLabel="name"
+                    placeholder="Select a City"
+                    className="flex-1"
+                  />{" "}
+                </div>
+                <div className="p-inputgroup flex-1">
+                  <span className="p-inputgroup-addon">
+                    <i className="pi pi-calendar-clock"></i>
+                  </span>
+                  <Calendar
+                    inputId="birth_date"
+                    value={fromDate}
+                    placeholder="From"
+                    className="flex-1"
+                    onChange={(e) => setFromDate(e.value)}
+                  />
+                </div>
+                <div className="p-inputgroup flex-1">
+                  <span className="p-inputgroup-addon">
+                    <i className="pi pi-calendar-clock"></i>
+                  </span>
+                  <Calendar
+                    inputId="birth_date"
+                    className="flex-1"
+                    placeholder="To"
+                    value={toDate}
+                    onChange={(e) => setToDate(e.value)}
+                  />
+                </div>
+
+                <Button label="Explore" className="" />
+              </div>
             </div>
             <div
               className={`px-6 py-4 ${
@@ -209,7 +261,6 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <div className="h-screen"></div>
       </div>
     </div>
   );
--- src/components/05-Header/Header.jsx
+++ src/components/05-Header/Header.jsx
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo/logoPng.png";
+import { Sidebar } from "primereact/sidebar";
+import { Button } from "primereact/button";
+
 import "./Header.css";
 
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [menuStatus, setMenuStatus] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  const [visibleRight, setVisibleRight] = useState(false);
 
   const handleNavigation = (path) => {
     navigate(path);
     window.scrollTo(0, 0);
-    setMenuStatus(false);
   };
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrolled(window.scrollY > 0);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -37,14 +35,9 @@ export default function Header() {
         scrolled ? "bg-[#eef0ee]" : "bg-transparent"
       }`}
     >
-      <div
-        className={`w-full flex justify-center items-center h-[70px] py-10 lg:px-40`}
-      >
+      <div className="w-full flex justify-center items-center h-[70px] py-10 lg:px-40">
         <div className="w-[85%] lg:w-full flex justify-between items-center px-6">
-          <div
-            className="hidden lg:flex flex-1 justify-end"
-            style={{ justifyContent: "space-evenly" }}
-          >
+          <div className="hidden lg:flex flex-1 justify-evenly">
             <div
               className="text-[16px] text-[#0067b6] cursor-pointer font-bold underline-animation"
               style={{ color: isActive("/") }}
@@ -65,10 +58,7 @@ export default function Header() {
             <img src={logo} alt="Explore Vacations" />
           </div>
 
-          <div
-            className="hidden lg:flex flex-1 justify-start"
-            style={{ justifyContent: "space-evenly" }}
-          >
+          <div className="hidden lg:flex flex-1 justify-evenly">
             <div
               className="text-[16px] text-[#0067b6] cursor-pointer font-bold underline-animation"
               style={{ color: isActive("/cars") }}
@@ -85,76 +75,47 @@ export default function Header() {
             </div>
           </div>
 
-          <div className="w-[0%] ml-14 flex lg:hidden justify-center items-center">
-            <button
-              className={`relative order-10 block self-center lg:hidden ${
-                menuStatus
-                  ? "visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(2)]:-rotate-45 [&_span:nth-child(3)]:w-0 "
-                  : ""
-              }`}
-              onClick={() => setMenuStatus(!menuStatus)}
-              aria-expanded={menuStatus}
-              aria-label="Toggle navigation"
-            >
-              <div className="absolute left-1/2 top-1/2 w-6 -translate-x-1/2 -translate-y-1/2 transform">
-                <span
-                  aria-hidden="true"
-                  className="absolute block h-0.5 w-9/12 -translate-y-2 transform rounded-full bg-white transition-all duration-300"
-                ></span>
-                <span
-                  aria-hidden="true"
-                  className="absolute block h-0.5 w-6 transform rounded-full bg-white transition duration-300"
-                ></span>
-                <span
-                  aria-hidden="true"
-                  className="absolute block h-0.5 w-1/2 origin-top-left translate-y-2 transform rounded-full bg-white transition-all duration-300"
-                ></span>
-              </div>
-            </button>
+          <div className="lg:hidden">
+            <Button
+              icon="pi pi-arrow-left"
+              onClick={() => setVisibleRight(true)}
+            />
           </div>
         </div>
       </div>
 
-      <div
-        className={`w-full h-[90vh] overflow-y-auto z-50 mt-[70px] fixed top-0 left-0 transition-all duration-500 ease-in-out bg-[#2ea3f2] transform ${
-          menuStatus
-            ? "translate-x-0 opacity-100 visible"
-            : "-translate-x-full opacity-0 invisible"
-        }`}
+      <Sidebar
+        visible={visibleRight}
+        position="right"
+        onHide={() => setVisibleRight(false)}
       >
-        <div className="flex flex-col items-center justify-start h-full">
-          <div className="w-[80%] mt-10">
-            <div
-              className="text-[16px] cursor-pointer py-3 font-semibold"
-              style={{ color: isActive("/") }}
-              onClick={() => handleNavigation("/")}
-            >
-              Home
-            </div>
-            <div
-              className="text-[16px] cursor-pointer py-3 font-semibold"
-              style={{ color: isActive("/tours") }}
-              onClick={() => handleNavigation("/tours")}
-            >
-              Tours
-            </div>
-            <div
-              className="text-[16px] cursor-pointer py-3 font-semibold"
-              style={{ color: isActive("/cars") }}
-              onClick={() => handleNavigation("/cars")}
-            >
-              Cars
-            </div>
-            <div
-              className="text-[16px] cursor-pointer py-3 font-semibold"
-              style={{ color: isActive("/contact") }}
-              onClick={() => handleNavigation("/contact")}
-            >
-              Contact
-            </div>
+        <div className="flex flex-col">
+          <div
+            className="text-[16px] cursor-pointer py-3 font-semibold"
+            onClick={() => handleNavigation("/")}
+          >
+            Home
+          </div>
+          <div
+            className="text-[16px] cursor-pointer py-3 font-semibold"
+            onClick={() => handleNavigation("/tours")}
+          >
+            Tours
+          </div>
+          <div
+            className="text-[16px] cursor-pointer py-3 font-semibold"
+            onClick={() => handleNavigation("/cars")}
+          >
+            Cars
+          </div>
+          <div
+            className="text-[16px] cursor-pointer py-3 font-semibold"
+            onClick={() => handleNavigation("/contact")}
+          >
+            Contact
           </div>
         </div>
-      </div>
+      </Sidebar>
     </div>
   );
 }
